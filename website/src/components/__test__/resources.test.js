67a65d9dcf7a169537a40b6eead05247a6d8a6f2
Patrícia Járosiová <patricia.jarosiova@student.tuke.sk>
2021-11-04 18:46:35 +0100

    1 test fixed

--- website/src/components/__test__/resources.test.js
+++ website/src/components/__test__/resources.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Resources from '../Resources/ResourcesComponent';
+import Resources from '../Resources/resourcesComponent';
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
